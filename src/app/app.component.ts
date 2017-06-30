c3f20aa6c8984880527e18f2a6dc96b849d702d5
Allan Farinas <awesomeplusplus@riseup.net>
2017-06-30 03:13:47 -0700

    UI changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Scale } from './scale';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   providers: [
     ChordProgGeneratorService,
     ScaleDataService
